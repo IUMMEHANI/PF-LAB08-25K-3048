00fad0986d66e3fb7bb1d7417b9258bd32e63bdc
UMM E HANI <k253048@nu.edu.pk>
2025-10-26 21:01:03 +0500

    Add files via upload

---
+++ task004.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int arr[2][2][2];
+    int i, j, k;
+    printf("Enter 8 elements for the 2x2x2 array:\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            for (k = 0; k < 2; k++) {
+                scanf("%d", &arr[i][j][k]);
+            }
+        }
+    }
+    printf("\nDisplaying the 2x2x2 3D array:\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            for (k = 0; k < 2; k++) {
+                printf("%d ", arr[i][j][k]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
